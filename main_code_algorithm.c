7605a3d5ec3e7032e4bcf945ccfa5e7a250b4047
Marissa <78126525+mars2699@users.noreply.github.com>
2021-10-24 14:24:27 -0400

    Add files via upload

---
+++ main_code_algorithm.c
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS  #
+
+# This is the main code. For now, I will
+# outline an algorithm to follow.
+
+
+# Parallelize least squares regression for predictors
+
+# Parallelize auto regression for predictors
+
+# Display/plot data to show best predictor
+
