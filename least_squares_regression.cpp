bca8b7bdd1b658f66f960f94223c589424e89b14
Marissa <78126525+mars2699@users.noreply.github.com>
2021-10-28 10:36:38 -0400

    Add files via upload

---
+++ least_squares_regression.cpp
@@ -0,0 +1,16 @@
+#include <iostream>     
+#include <fstream>      
+#include <vector>
+#include <string>
+#include <algorithm>    
+#include <iterator>     
+
+int main()
+{
+    string data("Bitcoin_data.csv");
+
+    string data['Marketcap'].unique();
+	
+	string crypto = 'BTC';
+	string currency = 'USD';
+}
